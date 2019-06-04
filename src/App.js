6096b994b2a77e6f37f78759e987c63f7900abc6
thierry saintmaxent <thierry.saintmaxent@decathlon.com>
2019-06-04 10:58:09 +0200

    app

--- src/App.js
+++ src/App.js
@@ -73,79 +73,77 @@ class App extends Component {
       <div className="App" >
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous" />
+          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossOrigin="anonymous" />
         </header>
-        <body>
-          <div class="container">
+          <div className="container">
             <form>
               <br />
-              <div class="row">
-                <input class="col-6" type="text" onChange={(e) => this.handleInputBrand("brand_1", e)} />
-                <div class="col-2" />
-                <input class="col-4" type="number" step="1" onChange={(e) => this.handleInputBrandValue("value_1", e)} />
+              <div className="row">
+                <input className="col-6" type="text" onChange={(e) => this.handleInputBrand("brand_1", e)} />
+                <div className="col-2" />
+                <input className="col-4" type="number" step="1" onChange={(e) => this.handleInputBrandValue("value_1", e)} />
               </div>
               <br />
-              <div class="row">
-                <input class="col-6" type="text" onChange={(e) => this.handleInputBrand("brand_2", e)} />
-                <div class="col-2" />
-                <input class="col-4" type="number" step="1" onChange={(e) => this.handleInputBrandValue("value_2", e)} />
+              <div className="row">
+                <input className="col-6" type="text" onChange={(e) => this.handleInputBrand("brand_2", e)} />
+                <div className="col-2" />
+                <input className="col-4" type="number" step="1" onChange={(e) => this.handleInputBrandValue("value_2", e)} />
               </div>
               <br />
-              <div class="row">
-                <input class="col-6" type="text" onChange={(e) => this.handleInputBrand("brand_3", e)} />
-                <div class="col-2" />
-                <input class="col-4" type="number" step="1" onChange={(e) => this.handleInputBrandValue("value_3", e)} />
+              <div className="row">
+                <input className="col-6" type="text" onChange={(e) => this.handleInputBrand("brand_3", e)} />
+                <div className="col-2" />
+                <input className="col-4" type="number" step="1" onChange={(e) => this.handleInputBrandValue("value_3", e)} />
               </div>
               <br />
-              <div class="row">
-                <input class="col-6" type="text" onChange={(e) => this.handleInputBrand("brand_4", e)} />
-                <div class="col-2" />
-                <input class="col-4" type="number" step="1" onChange={(e) => this.handleInputBrandValue("value_4", e)} />
+              <div className="row">
+                <input className="col-6" type="text" onChange={(e) => this.handleInputBrand("brand_4", e)} />
+                <div className="col-2" />
+                <input className="col-4" type="number" step="1" onChange={(e) => this.handleInputBrandValue("value_4", e)} />
               </div>
               <br />
-              <div class="row">
-                <input class="col-6" type="text" onChange={(e) => this.handleInputBrand("brand_5", e)} />
-                <div class="col-2" />
-                <input class="col-4" type="number" step="1" onChange={(e) => this.handleInputBrandValue("value_5", e)} />
+              <div className="row">
+                <input className="col-6" type="text" onChange={(e) => this.handleInputBrand("brand_5", e)} />
+                <div className="col-2" />
+                <input className="col-4" type="number" step="1" onChange={(e) => this.handleInputBrandValue("value_5", e)} />
               </div>
               <br />
-              <div class="row">
-                <input class="col-6" type="text" onChange={(e) => this.handleInputBrand("brand_6", e)} />
-                <div class="col-2" />
-                <input class="col-4" type="number" step="1" onChange={(e) => this.handleInputBrandValue("value_6", e)} />
+              <div className="row">
+                <input className="col-6" type="text" onChange={(e) => this.handleInputBrand("brand_6", e)} />
+                <div className="col-2" />
+                <input className="col-4" type="number" step="1" onChange={(e) => this.handleInputBrandValue("value_6", e)} />
               </div>
               <br />
-              <div class="row">
-                <input class="col-6" type="text" onChange={(e) => this.handleInputBrand("brand_7", e)} />
-                <div class="col-2" />
-                <input class="col-4" type="number" step="1" onChange={(e) => this.handleInputBrandValue("value_7", e)} />
+              <div className="row">
+                <input className="col-6" type="text" onChange={(e) => this.handleInputBrand("brand_7", e)} />
+                <div className="col-2" />
+                <input className="col-4" type="number" step="1" onChange={(e) => this.handleInputBrandValue("value_7", e)} />
               </div>
               <br />
-              <div class="row">
-                <input class="col-6" type="text" onChange={(e) => this.handleInputBrand("brand_8", e)} />
-                <div class="col-2" />
-                <input class="col-4" type="number" step="1" onChange={(e) => this.handleInputBrandValue("value_8", e)} />
+              <div className="row">
+                <input className="col-6" type="text" onChange={(e) => this.handleInputBrand("brand_8", e)} />
+                <div className="col-2" />
+                <input className="col-4" type="number" step="1" onChange={(e) => this.handleInputBrandValue("value_8", e)} />
               </div>
               <br />
-              <div class="row">
-                <input class="col-6" type="text" onChange={(e) => this.handleInputBrand("brand_9", e)} />
-                <div class="col-2" />
-                <input class="col-4" type="number" step="1" onChange={(e) => this.handleInputBrandValue("value_9", e)} />
+              <div className="row">
+                <input className="col-6" type="text" onChange={(e) => this.handleInputBrand("brand_9", e)} />
+                <div className="col-2" />
+                <input className="col-4" type="number" step="1" onChange={(e) => this.handleInputBrandValue("value_9", e)} />
               </div>
               <br />
-              <div class="row">
-                <input class="col-6" type="text" onChange={(e) => this.handleInputBrand("brand_10", e)} />
-                <div class="col-2" />
-                <input class="col-4" type="number" step="1" onChange={(e) => this.handleInputBrandValue("value_10", e)} />
+              <div className="row">
+                <input className="col-6" type="text" onChange={(e) => this.handleInputBrand("brand_10", e)} />
+                <div className="col-2" />
+                <input className="col-4" type="number" step="1" onChange={(e) => this.handleInputBrandValue("value_10", e)} />
               </div>
               <br />
-              <button class="button" onClick={this.saveFile}>Sauvegarder</button>
+              <button className="button" onClick={this.saveFile}>Sauvegarder</button>
             </form>
-            <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+            <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossOrigin="anonymous"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossOrigin="anonymous"></script>
+            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossOrigin="anonymous"></script>
           </div>
-        </body>
       </div >
 
     );
